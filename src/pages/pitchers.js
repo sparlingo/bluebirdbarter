192a591cea36e2a21ddb0ad8027fda52d847cc9d
Kevin Sparling <kevinsparling@gmail.com>
2020-02-25 23:35:58 +0800

    the basic, basic version for the pitchers page is up, hitters tomorrow

--- src/pages/pitchers.js
+++ src/pages/pitchers.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {graphql } from 'gatsby'
-import Image from 'gatsby-image'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 
@@ -8,21 +8,33 @@ export default ({data}) => {
   const pitchers = data.allSanityPitcher.edges
   return (
     <Layout>
-      <div id="allHitters" className="container">
+      <div id="allHitters" className="card-container">
         {pitchers.map(pitcher => 
           <div className="card box">
-            {pitcher.node.hand}
-            <div className="card-image">
-              <figure className="image is-2by3">
-
-              </figure>
-            </div>
-            <div className="content">
-              <p className="title is-3">{pitcher.node.person.name}</p>
+            <div className="card-content">
+              <div classname="media">
+                <div classname="media-left">
+                  <Img fixed={pitcher.node.person.profile.asset.fixed} />
+                </div>
+                <div className="media-content">
+                  <p className="title is-4">{pitcher.node.person.name}</p>
+                  <p className="subtitle is-6">{pitcher.node.person.slug.current}</p>
+                </div>
+              </div>
+              <div className="content">
+                <p>Look, this guy really sucked. Everyone liked him,
+                  but he was just not a good baseball player. You maybe have
+                  heard differently but take my word for it. This guy is/was
+                  an absolute garbage human being.
+                </p>
+              </div>
             </div>
             <footer className="card-footer">
               <div className="card-footer-item">
-
+                <span className="tag is-primary is-light is-large">Throws: {pitcher.node.hand}</span>
+              </div>
+              <div classname="card-footer-item">
+                <span className="tag is-info is-light is-large">Starter</span>
               </div>
             </footer>
           </div>
@@ -49,9 +61,9 @@ export const query = graphql`
             }
             profile {
               asset {
-                size
-                url
-                mimeType
+                fixed (width: 60){
+                  ...GatsbySanityImageFixed
+                }
               }
             }
           }
